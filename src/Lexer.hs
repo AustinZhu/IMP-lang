802daac601bd6c98b607909453333bb082ab4bc1
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-09-22 22:04:46 +0900

    add lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,2 +1,28 @@
 module Lexer where
 
+import Text.ParserCombinators.Parsec.Language
+import Text.Parsec.Token (GenLanguageDef, makeTokenParser, identifier, reserved, reservedOp, parens, whiteSpace, semi, integer)
+import Text.Parsec.Char (letter, alphaNum)
+
+langDef = emptyDef { 
+  identStart = letter,
+  identLetter = alphaNum,
+  reservedNames = ["true", "false", "skip", "if", "then", "else", "while", "do"],
+  reservedOpNames = ["+", "-", "*", "=", "<=", "!", "&", "|", "::=", ]
+}
+
+lexer = makeTokenParser langDef
+
+location = identifier lexer
+
+keyword = reserved lexer
+
+operator = reservedOp lexer
+
+space = whiteSpace lexer
+
+semicolon = semi lexer
+
+parentheses = parens lexer
+
+number = integer lexer
