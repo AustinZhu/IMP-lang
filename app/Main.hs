31dd0d92c5eb5f6884145da7ef7bc1bdd2c7b875
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-09-24 17:01:25 +0900

    improve command-line interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
+import Control.Monad (unless)
+import GHC.IO.Handle (hFlush)
 import Parser (impParser)
 import Syntax (Com)
+import System.IO (stdout)
 import Text.Parsec.Prim (parse)
 
 parseIMP :: String -> Com
@@ -9,7 +12,11 @@ parseIMP str = case parse impParser "" str of
   Left e -> error $ show e
   Right r -> r
 
-main :: IO String
+main :: IO ()
 main = do
-  ast <- parseIMP <$> getLine
-  return $ show ast
+  putStr "IMP> "
+  hFlush stdout
+  code <- getLine
+  unless (code == "\\q") $ do
+    print $ parseIMP code
+    main
